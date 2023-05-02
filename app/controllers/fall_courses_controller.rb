fca996b5fb27d4f6b02f46016b929a44880bb372
lhamdy <leenahohamdy@gmail.com>
2023-05-02 19:05:48 -0400

    added course views

--- app/controllers/fall_courses_controller.rb
+++ app/controllers/fall_courses_controller.rb
@@ -1,2 +1,29 @@
 class FallCoursesController < ApplicationController
+    def index
+        @fall_courses = FallCourse.all
+    end
+
+    def show
+        @fall_course = FallCourse.find(params[:id])
+    end
+
+    def new 
+        @fall_course = FallCourse.new
+    end
+
+    def create
+        @fall_course = FallCourse.new(fall_course_params)
+
+        if @fall_course.save 
+            redirect_to @fall_course
+        else
+            render :new, status: :unprocessable_entity
+
+        end
+    end
+
+    private
+        def fall_course_params
+            params.require(:fall_course).permit(:cid, :title, :instructor, :credits, :prereqs, :description)
+        end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,11 @@
 Rails.application.routes.draw do
+  root "fall_courses#index"
   resources :previous_grade_data
   resources :fall_courses
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
+  
+  get "/fall_courses", to: "fall_courses#index"
+  get "/fall_courses/:id", to: "fall_courses#show"
   # Defines the root path route ("/")
   # root "articles#index"
 end
