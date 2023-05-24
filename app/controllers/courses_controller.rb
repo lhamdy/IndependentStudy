54376b15a9a41ea6f1db9db216240a5870c0632e
lhamdy <leenahohamdy@gmail.com>
2023-05-24 16:09:12 -0400

    added comments, cleaned up some code

--- app/controllers/courses_controller.rb
+++ app/controllers/courses_controller.rb
@@ -1,9 +1,3 @@
-# class CoursesController < ApplicationController
-#   def index
-#   end
-# end
-
-
 class CoursesController < ApplicationController
   def index
       @courses = Course.all
@@ -30,18 +24,18 @@ class CoursesController < ApplicationController
 
   def edit
     @course = Course.find(params[:id])
-    puts "gonna edit"
+    
   end
 
   def update
-    puts "entered update"
+    
     @course = Course.find(params[:id])
-    puts "found grade"
+    
     if @course.update(course_params)
-      puts "making update"
+      
       redirect_to @course
     else
-      puts "failed to update"
+      
       render :edit, status: :unprocessable_entity
     end
   end
--- app/controllers/grades_controller.rb
+++ app/controllers/grades_controller.rb
@@ -1,6 +1,3 @@
-# class GradesController < ApplicationController
-# end
-
 class GradesController < ApplicationController
     def index  
         @grades = Grade.all
@@ -10,12 +7,7 @@ class GradesController < ApplicationController
         puts params[:sid]
         @student = Grade.find_by(sid: params[:sid])
         @grades_ = Grade.where(sid: params[:sid])
-        # if @student.nil?
-        # # Handle the case where the student with the given sid is not found
-        #     redirect_to grades_path, notice: "Student not found"
-        # else
-        #     @courses = Grade.where(sid: params[:sid])
-        # end
+       
     end
 
     def new 
@@ -37,13 +29,6 @@ class GradesController < ApplicationController
             end
         end
 
-        # if @grade.save 
-        #     redirect_to grade_path(@grade.sid)
-            
-        # else
-        #     render :new, status: :unprocessable_entity
-
-        # end
     end
 
     def show_checkboxes
